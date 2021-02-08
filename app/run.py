6266115ad54531c4d711841e88c831e936577323
Rehman Arshad <rehman.arshad777@gmail.com>
2021-02-08 17:53:37 -0500

    removed pycache, updated .gitignore, added backend code for flash messages!

--- app/run.py
+++ app/run.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
 
 
@@ -30,7 +31,7 @@ posts = [
 
 @app.route('/')
 @app.route('/home')
-def home_page():
+def home():
     return render_template('home.html', posts=posts)
 
 
@@ -39,9 +40,12 @@ def about_page():
     return render_template('about.html', title='About')
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')      # displays success message in bootstrap green! 
+        return redirect(url_for('home'))                                    # redirects us to the home page! 
     return render_template('register.html', title='Register', form=form)
 
 
